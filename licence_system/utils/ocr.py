7d50b44366355fa5a59324610b62ec528be6f54a
Daniel <84752451+danielangeloni@users.noreply.github.com>
2023-10-22 15:24:04 +1100

    Continue working on OCR

--- licence_system/utils/ocr.py
+++ licence_system/utils/ocr.py
@@ -18,15 +18,151 @@ def ocr_with_coordinates(image: np.ndarray, coordinates: Tuple) -> str:
     Returns:
         str: ocr result
     """
+    # x, y, width, height = coordinates
+
+    # # Crop the image
+    # cropped_image = image[y : y + height, x : x + width]
+    # cropped_image = cv2.cvtColor(cropped_image, cv2.COLOR_BGR2GRAY)
+
+    # cropped_image = cv2.convertScaleAbs(cropped_image, alpha=1.2, beta=30)
+    # # cropped_image = cv2.convertScaleAbs(cropped_image, alpha=2.0, beta=30)
+
+    # # # Convert to greyscale
+
+    # # # Binarization
+    # _, binary = cv2.threshold(
+    #     cropped_image, 0, 300, cv2.THRESH_BINARY + cv2.THRESH_OTSU
+    # )
+
+    # # # Resize for better accuracy
+    # binary = cv2.resize(binary, None, fx=10, fy=10, interpolation=cv2.INTER_LINEAR)
+
+    # # # binary = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8)).apply(binary)
+
+    # # cv2.imshow("Image", binary)
+    # # cv2.imshow("Image", cropped_image)
+    # # cv2.waitKey(0)
+
+    # # Extract text
+    # # psm=8, 6
+    # extracted_text = pytesseract.image_to_string(
+    #     cropped_image,
+    #     lang="eng",
+    #     config="--oem 3 --psm 8 -c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+    # )
+
+    # return extracted_text
+
+    # x, y, width, height = coordinates
+
+    # # Crop the image
+    # cropped_image = image[y : y + height, x : x + width]
+    # cropped_image = cv2.cvtColor(cropped_image, cv2.COLOR_BGR2GRAY)
+    # cropped_image = cv2.convertScaleAbs(cropped_image, alpha=1.2, beta=30)
+
+    # # Sharpening
+    # kernel = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
+    # sharpened = cv2.filter2D(cropped_image, -1, kernel)
+
+    # # Binarization
+    # _, binary = cv2.threshold(sharpened, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+    # # Resize for better accuracy
+    # # binary = cv2.resize(binary, None, fx=10, fy=10, interpolation=cv2.INTER_LINEAR)
+    # # binary = cv2.resize(binary, None, fx=1, fy=1, interpolation=cv2.INTER_LINEAR)
+
+    # cv2.imshow("Image", binary)
+    # cv2.waitKey(0)
+
+    # # Extract text
+    # extracted_text = pytesseract.image_to_string(
+    #     binary,
+    #     lang="eng",
+    #     config="--oem 3 --psm 8 -c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+    # )
+
+    # return extracted_text.strip()
+
+    # x, y, width, height = coordinates
+
+    # # Crop the image
+    # cropped_image = image[y : y + height, x : x + width]
+    # cropped_image = cv2.cvtColor(cropped_image, cv2.COLOR_BGR2GRAY)
+
+    # # Mild Sharpening
+    # kernel = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
+    # sharpened = cv2.filter2D(cropped_image, -1, kernel)
+
+    # # Binarization using Adaptive Thresholding
+    # binary = cv2.adaptiveThreshold(
+    #     sharpened, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2
+    # )
+
+    # # binary = cv2.resize(binary, None, fx=2, fy=2, interpolation=cv2.INTER_LINEAR)
+
+    # cv2.imshow("Image", binary)
+    # cv2.waitKey(0)
+
+    # # Extract text
+    # extracted_text = pytesseract.image_to_string(
+    #     binary,
+    #     lang="eng",
+    #     config="--oem 3 --psm 8 -c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+    # )
+
+    # return extracted_text.strip()
+
+    # x, y, width, height = coordinates
+
+    # # Crop the image
+    # cropped_image = image[y:y+height, x:x+width]
+    # cropped_image = cv2.cvtColor(cropped_image, cv2.COLOR_BGR2GRAY)
+
+    # cropped_image = cv2.convertScaleAbs(cropped_image, alpha=1.6, beta=20)
+
+    # # Adaptive Thresholding
+    # binary = cv2.adaptiveThreshold(cropped_image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+    #                                cv2.THRESH_BINARY, 11, 2)
+
+    # cv2.imshow("Image", binary)
+    # cv2.waitKey(0)
+
+    # # Extract text
+    # extracted_text = pytesseract.image_to_string(
+    #     binary,
+    #     lang="eng",
+    #     config="--oem 3 --psm 8 -c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    # )
+
+    # return extracted_text.strip()
+
     x, y, width, height = coordinates
 
     # Crop the image
     cropped_image = image[y : y + height, x : x + width]
+    cropped_image = cv2.cvtColor(cropped_image, cv2.COLOR_BGR2GRAY)
 
-    # cv2.imshow("Image", cropped_image)
+    cropped_image = cv2.convertScaleAbs(cropped_image, alpha=0.5, beta=30)
+    # cropped_image = cv2.convertScaleAbs(cropped_image, alpha=2.0, beta=30)
+
+    # # Convert to greyscale
+
+    # # Binarization
+    _, binary = cv2.threshold(
+        cropped_image, 0, 300, cv2.THRESH_BINARY + cv2.THRESH_OTSU
+    )
+
+    # # Resize for better accuracy
+    binary = cv2.resize(binary, None, fx=10, fy=10, interpolation=cv2.INTER_LINEAR)
+
+    # # binary = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8)).apply(binary)
+
+    # cv2.imshow("Image", binary)
+    # # cv2.imshow("Image", cropped_image)
     # cv2.waitKey(0)
 
     # Extract text
+    # psm=8, 6
     extracted_text = pytesseract.image_to_string(
         cropped_image,
         lang="eng",
--- tests/ocr/test_ocr.py
+++ tests/ocr/test_ocr.py
@@ -2,7 +2,6 @@
 
 import cv2
 import pytest
-
 from licence_system.utils.ocr import ocr_with_coordinates
 
 
@@ -11,9 +10,11 @@ from licence_system.utils.ocr import ocr_with_coordinates
     "image_path, coordinates, expected_text",
     # (path_to_img, (x, y, width, height), expected_text)
     [
-        ("./tests/ocr/1.jpg", (90, 40, 720, 280), "SEB384"),
-        ("./tests/ocr/1.jpg", (90, 300, 720, 280), "600063"),
-        ("./tests/ocr/1.jpg", (90, 600, 720, 280), "NOA770"),
+        ("./tests/ocr/1.jpg", (30, 10, 252, 97), "SEB384"),
+        ("./tests/ocr/1.jpg", (30, 100, 252, 97), "600063"),
+        ("./tests/ocr/1.jpg", (35, 190, 252, 97), "NOA770"),
+        # ("./tests/ocr/1.jpg", (30, 190, 252, 97), "NOA770"),
+        ("./tests/ocr/2.jpg", (165, 295, 57, 35), "BE33TA"),
     ],
 )
 def test_ocr_with_coordinates(image_path, coordinates, expected_text):
