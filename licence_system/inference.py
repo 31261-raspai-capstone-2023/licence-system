0697a3313ecc0057796bf7414520e84fd74dfa2a
daniel <daniel@daniel.com>
2023-11-08 17:10:36 +0900

    Turn inference into module @erencanpelin

--- licence_system/inference.py
+++ licence_system/inference.py
@@ -14,70 +14,59 @@ from PIL import Image, ImageDraw
 from licence_system.utils.data_loader import show_imgs
 from licence_system.utils.model_class import LPLocalNet
 
-PATH = "licence_system/models/checkpoints/LPLocalNet_B250_E500_LR0.0010_Acc74.22.pth"
-IMAGE_PATH = "inference-images"
-
 faulthandler.enable()
 
-print(f"Loading Model: {PATH}")
-model = LPLocalNet()
-state_dict = torch.load(PATH, map_location='cpu')
-model.load_state_dict(state_dict)
-model.eval()
-print("Successfully loaded model!")
+class Inference():
+    PATH = "licence_system/models/checkpoints/LPLocalNet_B250_E500_LR0.0010_Acc74.22.pth" #path of ML model
+    DISPLAY_OUTPUT = False # when true, shows a window with the output of the original image cropped to the detected bounding box
+    BBOX_BUFFER = 60 # size to add around the detected bounding box for safety, should be relative to the original image size
+    #IMAGE_PATH = "inference-images"
 
-# read all images for inference
-print("Loading Images...")
-images = []
-for file in os.listdir(IMAGE_PATH):
-    if file.endswith(".png") or file.endswith(".jpg") or file.endswith(".jpeg"):
-        images.append(os.path.join(IMAGE_PATH, file))
+    def __init__(self):
+        print(f"Loading Model: {PATH}")
+        self.model = LPLocalNet()
+        state_dict = torch.load(PATH, map_location='cpu')
+        self.model.load_state_dict(state_dict)
+        self.model.eval()
+        print("Successfully loaded model!")
 
-print("Successfully loaded images!")
+    def Infer(self, selected_img):
+        with Image.open(selected_img).convert("L") as img:
+            resized_img = img.copy().resize((416, 416))
+            numpy_data = np.array(resized_img)
+            print("Preprocessing image...")
+            # print(numpy_data)
+            X = torch.Tensor(numpy_data)
+            X = X / 255.0
+            print("Passing image into model...")
+            model_in = X.view(-1, 1, 416, 416)
+            print(model_in)
+            net_out = self.model(model_in).detach().cpu()[0]
+            print("Input Shape:", model_in.shape)
+            print(f"Estimate bounding box: {net_out}")
+            
+            bbox = net_out
+            imgcp = img.copy()
+            
+            # resize output bounding to fix og image
+            original_width, original_height = imgcp.size  # Replace with your original image dimensions
+            scaling_factor_width = original_width / 416
+            scaling_factor_height = original_height / 416
 
-img_index = random.randrange(0, len(images))
-selected_img = images[2]
-print(f"Opening image {selected_img}...")
-DISPLAY_OUTPUT = True
-BBOX_BUFFER = 60
-with Image.open(selected_img).convert("L") as img:
-    resized_img = img.copy().resize((416, 416))
-    numpy_data = np.array(resized_img)
-    print("Preprocessing image...")
-    # print(numpy_data)
-    X = torch.Tensor(numpy_data)
-    X = X / 255.0
-    print("Passing image into model...")
-    model_in = X.view(-1, 1, 416, 416)
-    print(model_in)
-    net_out = model(model_in)
-    print("Input Shape:", model_in.shape)
-    out1 = model.conv1(model_in)
-    print("After conv1:", out1.shape)
-    print(f"Estimate bounding box: {net_out.detach().cpu()}")
-    
-    #displaying output
-    print("displaying output")
-    if DISPLAY_OUTPUT is True:
-        bbox=net_out.detach().cpu()[0]
-        #bbox = torch.round(bbox).int()
-        print("bounding box: ", bbox)
-        imgcp = img.copy()
-        
-        # resize output bounding to fix og image
-        original_width, original_height = imgcp.size  # Replace with your original image dimensions
-        scaling_factor_width = original_width / 416
-        scaling_factor_height = original_height / 416
-
-        x1 = bbox[0].item() * scaling_factor_width - BBOX_BUFFER
-        y1 = bbox[1].item() * scaling_factor_height - BBOX_BUFFER
-        x2 = bbox[2].item() * scaling_factor_width + BBOX_BUFFER
-        y2 = bbox[3].item() * scaling_factor_height + BBOX_BUFFER
-        
-        # draw bounding box onto img
-        #imgcp_draw = ImageDraw.Draw(imgcp)
-        #imgcp_draw.rectangle([x1,y1,x2,y2], fill = None, outline = "white", width=7)
-        cropped_img = imgcp.crop((x1, y1, x2, y2))
-        
-        # cropped output gets fed into OCR code
-        cropped_img.show()
+            x1 = bbox[0].item() * scaling_factor_width - self.BBOX_BUFFER
+            y1 = bbox[1].item() * scaling_factor_height - self.BBOX_BUFFER
+            x2 = bbox[2].item() * scaling_factor_width + self.BBOX_BUFFER
+            y2 = bbox[3].item() * scaling_factor_height + self.BBOX_BUFFER
+            
+            # draw bounding box onto img
+            #imgcp_draw = ImageDraw.Draw(imgcp)
+            #imgcp_draw.rectangle([x1,y1,x2,y2], fill = None, outline = "white", width=7)
+            cropped_img = imgcp.crop((x1, y1, x2, y2))
+            
+            #displaying output
+            print("displaying output")
+            if self.DISPLAY_OUTPUT is True:
+                # cropped output gets fed into OCR code
+                cropped_img.show()
+                
+            return cropped_img
