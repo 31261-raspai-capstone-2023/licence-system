a18e4ca96dc6217e1f2fbac7cec00af4334a166c
Daniel <84752451+danielangeloni@users.noreply.github.com>
2023-11-08 18:50:14 +1100

    IT WORKS

--- licence_system/utils/ocr.py
+++ licence_system/utils/ocr.py
@@ -15,6 +15,39 @@ import os
 import imutils
 
 
+def find_and_crop_square(image):
+    # Set a threshold to identify black (or very dark) regions in the image
+    _, thresh = cv2.threshold(image, 15, 255, cv2.THRESH_BINARY_INV)
+
+    # Find contours
+    contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Look for the largest rectangle in the contours
+    largest_area = 0
+    largest_rectangle = None
+
+    for cnt in contours:
+        # Calculate the bounding rectangle
+        x, y, w, h = cv2.boundingRect(cnt)
+        area = w * h
+
+        # Choose the largest area which is more likely to be our target rectangle
+        if area > largest_area:
+            largest_area = area
+            largest_rectangle = (x, y, w, h)
+
+    if largest_rectangle is not None:
+        x, y, w, h = largest_rectangle
+
+        # Crop the image according to the found rectangle
+        cropped_image = image[y : y + h, x : x + w]
+
+        # Save or return the cropped image
+        return cropped_image
+    else:
+        raise Exception("S")
+
+
 def increase_contrast(image):
     # Convert to YUV
     image_yuv = cv2.cvtColor(image, cv2.COLOR_BGR2YUV)
@@ -84,29 +117,29 @@ pytesseract.pytesseract.tesseract_cmd = tesseract_cmd
 
 def preprocess_image(image):
     # deskewed_image = deskew_image(image)
-    # # Convert the image to grayscale
-    # binary_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # Convert the image to grayscale
+    binary_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    binary_image = increase_contrast(image)
+    # binary_image = increase_contrast(image)
 
-    # Resize for better accuracy
-    width = int(binary_image.shape[1] * 2)
-    height = int(binary_image.shape[0] * 2)
-    dimensions = (width, height)
+    # # Resize for better accuracy
+    # width = int(binary_image.shape[1] * 2)
+    # height = int(binary_image.shape[0] * 2)
+    # dimensions = (width, height)
 
-    binary_image = cv2.resize(binary_image, dimensions, interpolation=cv2.INTER_CUBIC)
-    binary_image = binarize_image(binary_image)
-    binary_image = remove_noise(binary_image)
-    binary_image = deskew_image(binary_image)
+    # binary_image = cv2.resize(binary_image, dimensions, interpolation=cv2.INTER_CUBIC)
+    # binary_image = binarize_image(binary_image)
+    # binary_image = remove_noise(binary_image)
+    # binary_image = deskew_image(binary_image)
 
     # # # Use GaussianBlur to reduce noise and improve OCR accuracy
-    # # blurred_image = cv2.GaussianBlur(gray_image, (5, 5), 0)
+    # blurred_image = cv2.GaussianBlur(binary_image, (5, 5), 0)
 
     # # # Apply Non-local Means Denoising
-    # # blurred_image = cv2.fastNlMeansDenoising(gray_image, None, 5, 7, 21)
+    blurred_image = cv2.fastNlMeansDenoising(binary_image, None, 5, 7, 21)
 
     # Instead of GaussianBlur, let's use a median blur which preserves edges better
-    blurred_image = cv2.medianBlur(binary_image, 3)
+    # blurred_image = cv2.medianBlur(binary_image, 3)
 
     # Adaptive thresholding - this considers small regions of the image to adaptively
     # change the threshold and might work better with varying lighting conditions
@@ -126,8 +159,11 @@ def preprocess_image(image):
 
     cv2.imshow("Image", binary_image)
     cv2.waitKey(0)
+    cropped_image = find_and_crop_square(binary_image)
+    cv2.imshow("Image", cropped_image)
+    cv2.waitKey(0)
 
-    return binary_image
+    return cropped_image
 
     # # Convert the image to grayscale
     # gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -155,14 +191,26 @@ def preprocess_image(image):
 
 
 def extract_license_plate_text(preprocessed_image):
-    # Use Tesseract to do OCR on the preprocessed image
-    custom_config = r"--oem 3 --psm 8"  # You might need to adjust the psm value based on your images
-    text = pytesseract.image_to_string(preprocessed_image, config=custom_config)
+    # # Use Tesseract to do OCR on the preprocessed image
+    # custom_config = r"--oem 3 --psm 8"  # You might need to adjust the psm value based on your images
+    # text = pytesseract.image_to_string(preprocessed_image, config=custom_config)
 
-    # Clean up the text
-    text = "".join(e for e in text if e.isalnum())
+    # # Clean up the text
+    # text = "".join(e for e in text if e.isalnum())
+    texts = []
+    for i in range(0, 13):
+        try:
+            custom_config = f"--oem 3 --psm {i} -c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"  # You might need to adjust the psm value based on your images
+            text = pytesseract.image_to_string(
+                preprocessed_image,
+                config=custom_config,
+                lang="eng",
+            )
+            texts.append(text)
+        except:
+            pass
 
-    return text
+    return texts
 
 
 def ocr_with_coordinates(image: np.ndarray, coordinates: Tuple) -> str:
@@ -177,176 +225,177 @@ def ocr_with_coordinates(image: np.ndarray, coordinates: Tuple) -> str:
     """
     # x, y, width, height = coordinates
 
+    # # # Crop the image
+    # # cropped_image = image[y : y + height, x : x + width]
+    # # cropped_image = cv2.cvtColor(cropped_image, cv2.COLOR_BGR2GRAY)
+
+    # # cropped_image = cv2.convertScaleAbs(cropped_image, alpha=1.2, beta=30)
+    # # # cropped_image = cv2.convertScaleAbs(cropped_image, alpha=2.0, beta=30)
+
+    # # # # Convert to greyscale
+
+    # # # # Binarization
+    # # _, binary = cv2.threshold(
+    # #     cropped_image, 0, 300, cv2.THRESH_BINARY + cv2.THRESH_OTSU
+    # # )
+
+    # # # # Resize for better accuracy
+    # # binary = cv2.resize(binary, None, fx=10, fy=10, interpolation=cv2.INTER_LINEAR)
+
+    # # # # binary = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8)).apply(binary)
+
+    # # # cv2.imshow("Image", binary)
+    # # # cv2.imshow("Image", cropped_image)
+    # # # cv2.waitKey(0)
+
+    # # # Extract text
+    # # # psm=8, 6
+    # # extracted_text = pytesseract.image_to_string(
+    # #     cropped_image,
+    # #     lang="eng",
+    # #     config="--oem 3 --psm 8 -c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+    # # )
+
+    # # return extracted_text
+
+    # # x, y, width, height = coordinates
+
+    # # # Crop the image
+    # # cropped_image = image[y : y + height, x : x + width]
+    # # cropped_image = cv2.cvtColor(cropped_image, cv2.COLOR_BGR2GRAY)
+    # # cropped_image = cv2.convertScaleAbs(cropped_image, alpha=1.2, beta=30)
+
+    # # # Sharpening
+    # # kernel = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
+    # # sharpened = cv2.filter2D(cropped_image, -1, kernel)
+
+    # # # Binarization
+    # # _, binary = cv2.threshold(sharpened, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+    # # # Resize for better accuracy
+    # # # binary = cv2.resize(binary, None, fx=10, fy=10, interpolation=cv2.INTER_LINEAR)
+    # # # binary = cv2.resize(binary, None, fx=1, fy=1, interpolation=cv2.INTER_LINEAR)
+
+    # # cv2.imshow("Image", binary)
+    # # cv2.waitKey(0)
+
+    # # # Extract text
+    # # extracted_text = pytesseract.image_to_string(
+    # #     binary,
+    # #     lang="eng",
+    # #     config="--oem 3 --psm 8 -c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+    # # )
+
+    # # return extracted_text.strip()
+
+    # # x, y, width, height = coordinates
+
+    # # # Crop the image
+    # # cropped_image = image[y : y + height, x : x + width]
+    # # cropped_image = cv2.cvtColor(cropped_image, cv2.COLOR_BGR2GRAY)
+
+    # # # Mild Sharpening
+    # # kernel = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
+    # # sharpened = cv2.filter2D(cropped_image, -1, kernel)
+
+    # # # Binarization using Adaptive Thresholding
+    # # binary = cv2.adaptiveThreshold(
+    # #     sharpened, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2
+    # # )
+
+    # # # binary = cv2.resize(binary, None, fx=2, fy=2, interpolation=cv2.INTER_LINEAR)
+
+    # # cv2.imshow("Image", binary)
+    # # cv2.waitKey(0)
+
+    # # # Extract text
+    # # extracted_text = pytesseract.image_to_string(
+    # #     binary,
+    # #     lang="eng",
+    # #     config="--oem 3 --psm 8 -c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+    # # )
+
+    # # return extracted_text.strip()
+
+    # # x, y, width, height = coordinates
+
+    # # # Crop the image
+    # # cropped_image = image[y:y+height, x:x+width]
+    # # cropped_image = cv2.cvtColor(cropped_image, cv2.COLOR_BGR2GRAY)
+
+    # # cropped_image = cv2.convertScaleAbs(cropped_image, alpha=1.6, beta=20)
+
+    # # # Adaptive Thresholding
+    # # binary = cv2.adaptiveThreshold(cropped_image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+    # #                                cv2.THRESH_BINARY, 11, 2)
+
+    # # cv2.imshow("Image", binary)
+    # # cv2.waitKey(0)
+
+    # # # Extract text
+    # # extracted_text = pytesseract.image_to_string(
+    # #     binary,
+    # #     lang="eng",
+    # #     config="--oem 3 --psm 8 -c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    # # )
+
+    # # return extracted_text.strip()
+
+    # x, y, width, height = coordinates
+
     # # Crop the image
     # cropped_image = image[y : y + height, x : x + width]
     # cropped_image = cv2.cvtColor(cropped_image, cv2.COLOR_BGR2GRAY)
 
+    # # cropped_image = cv2.convertScaleAbs(cropped_image, alpha=0.5, beta=30)
     # cropped_image = cv2.convertScaleAbs(cropped_image, alpha=1.2, beta=30)
     # # cropped_image = cv2.convertScaleAbs(cropped_image, alpha=2.0, beta=30)
+
+    # # https://yashlahoti.medium.com/number-plate-recognition-in-python-using-tesseract-ocr-cc15853aca36
+    # # https://github.com/kangsunghyun111/Car-license-plate-recognition-using-tesseract-OCR/blob/master/main.cpp
+    # # https://stackoverflow.com/questions/66935787/why-does-tesseract-not-recognize-the-text-on-the-licence-plate-while-easyocr-doe
+    # # https://stackoverflow.com/questions/55349307/how-to-tune-tesseract-for-identifying-number-plate-of-a-car-more-accurately
+    # # https://stackoverflow.com/questions/72381645/python-tesseract-license-plate-recognition
+    # # https://www.google.com/search?q=teseract+ocr+on+blurry+licence+plate+image
+    # cropped_image = cv2.bilateralFilter(cropped_image, 100, 5, 5)
+    # # cropped_image = cv2.Canny(cropped_image, 10, 5) # no
+    # # contours = cv2.findContours(cropped_image.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    # # contours = imutils.grab_contours(contours)
+    # # contours = sorted(contours, key = cv2.contourArea, reverse = True)[:10]
+
+    # # # Sharpening
+    # # kernel = np.array([[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]])
+    # # cropped_image = cv2.filter2D(cropped_image, -1, kernel)
 
     # # # Convert to greyscale
 
     # # # Binarization
-    # _, binary = cv2.threshold(
-    #     cropped_image, 0, 300, cv2.THRESH_BINARY + cv2.THRESH_OTSU
-    # )
+    # # _, binary = cv2.threshold(
+    # #     cropped_image, 0, 300, cv2.THRESH_BINARY + cv2.THRESH_OTSU
+    # # )
 
-    # # # Resize for better accuracy
-    # binary = cv2.resize(binary, None, fx=10, fy=10, interpolation=cv2.INTER_LINEAR)
+    # #
 
-    # # # binary = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8)).apply(binary)
-
-    # # cv2.imshow("Image", binary)
+    # # # cv2.imshow("Image", binary)
     # # cv2.imshow("Image", cropped_image)
     # # cv2.waitKey(0)
 
-    # # Extract text
-    # # psm=8, 6
-    # extracted_text = pytesseract.image_to_string(
-    #     cropped_image,
-    #     lang="eng",
-    #     config="--oem 3 --psm 8 -c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
-    # )
-
+    # # # Extract text
+    # # # psm=8, 6
+    # # extracted_text = pytesseract.image_to_string(
+    # #     cropped_image,
+    # #     lang="eng",
+    # #     config="--oem 3 --psm 7 -c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+    # #     # config="--oem 3 --psm 11 -c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+    # # )
     # return extracted_text
 
-    # x, y, width, height = coordinates
-
-    # # Crop the image
-    # cropped_image = image[y : y + height, x : x + width]
-    # cropped_image = cv2.cvtColor(cropped_image, cv2.COLOR_BGR2GRAY)
-    # cropped_image = cv2.convertScaleAbs(cropped_image, alpha=1.2, beta=30)
-
-    # # Sharpening
-    # kernel = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
-    # sharpened = cv2.filter2D(cropped_image, -1, kernel)
-
-    # # Binarization
-    # _, binary = cv2.threshold(sharpened, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-
-    # # Resize for better accuracy
-    # # binary = cv2.resize(binary, None, fx=10, fy=10, interpolation=cv2.INTER_LINEAR)
-    # # binary = cv2.resize(binary, None, fx=1, fy=1, interpolation=cv2.INTER_LINEAR)
-
-    # cv2.imshow("Image", binary)
-    # cv2.waitKey(0)
-
-    # # Extract text
-    # extracted_text = pytesseract.image_to_string(
-    #     binary,
-    #     lang="eng",
-    #     config="--oem 3 --psm 8 -c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
-    # )
-
-    # return extracted_text.strip()
-
-    # x, y, width, height = coordinates
-
-    # # Crop the image
-    # cropped_image = image[y : y + height, x : x + width]
-    # cropped_image = cv2.cvtColor(cropped_image, cv2.COLOR_BGR2GRAY)
-
-    # # Mild Sharpening
-    # kernel = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
-    # sharpened = cv2.filter2D(cropped_image, -1, kernel)
-
-    # # Binarization using Adaptive Thresholding
-    # binary = cv2.adaptiveThreshold(
-    #     sharpened, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2
-    # )
-
-    # # binary = cv2.resize(binary, None, fx=2, fy=2, interpolation=cv2.INTER_LINEAR)
-
-    # cv2.imshow("Image", binary)
-    # cv2.waitKey(0)
-
-    # # Extract text
-    # extracted_text = pytesseract.image_to_string(
-    #     binary,
-    #     lang="eng",
-    #     config="--oem 3 --psm 8 -c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
-    # )
-
-    # return extracted_text.strip()
-
-    # x, y, width, height = coordinates
-
-    # # Crop the image
-    # cropped_image = image[y:y+height, x:x+width]
-    # cropped_image = cv2.cvtColor(cropped_image, cv2.COLOR_BGR2GRAY)
-
-    # cropped_image = cv2.convertScaleAbs(cropped_image, alpha=1.6, beta=20)
-
-    # # Adaptive Thresholding
-    # binary = cv2.adaptiveThreshold(cropped_image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-    #                                cv2.THRESH_BINARY, 11, 2)
-
-    # cv2.imshow("Image", binary)
-    # cv2.waitKey(0)
-
-    # # Extract text
-    # extracted_text = pytesseract.image_to_string(
-    #     binary,
-    #     lang="eng",
-    #     config="--oem 3 --psm 8 -c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-    # )
-
-    # return extracted_text.strip()
-
     x, y, width, height = coordinates
-
-    # Crop the image
-    cropped_image = image[y : y + height, x : x + width]
-    cropped_image = cv2.cvtColor(cropped_image, cv2.COLOR_BGR2GRAY)
-
-    # cropped_image = cv2.convertScaleAbs(cropped_image, alpha=0.5, beta=30)
-    cropped_image = cv2.convertScaleAbs(cropped_image, alpha=1.2, beta=30)
-    # cropped_image = cv2.convertScaleAbs(cropped_image, alpha=2.0, beta=30)
-
-    # https://yashlahoti.medium.com/number-plate-recognition-in-python-using-tesseract-ocr-cc15853aca36
-    # https://github.com/kangsunghyun111/Car-license-plate-recognition-using-tesseract-OCR/blob/master/main.cpp
-    # https://stackoverflow.com/questions/66935787/why-does-tesseract-not-recognize-the-text-on-the-licence-plate-while-easyocr-doe
-    # https://stackoverflow.com/questions/55349307/how-to-tune-tesseract-for-identifying-number-plate-of-a-car-more-accurately
-    # https://stackoverflow.com/questions/72381645/python-tesseract-license-plate-recognition
-    # https://www.google.com/search?q=teseract+ocr+on+blurry+licence+plate+image
-    cropped_image = cv2.bilateralFilter(cropped_image, 100, 5, 5)
-    # cropped_image = cv2.Canny(cropped_image, 10, 5) # no
-    # contours = cv2.findContours(cropped_image.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    # contours = imutils.grab_contours(contours)
-    # contours = sorted(contours, key = cv2.contourArea, reverse = True)[:10]
-
-    # # Sharpening
-    # kernel = np.array([[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]])
-    # cropped_image = cv2.filter2D(cropped_image, -1, kernel)
-
-    # # Convert to greyscale
-
-    # # Binarization
-    # _, binary = cv2.threshold(
-    #     cropped_image, 0, 300, cv2.THRESH_BINARY + cv2.THRESH_OTSU
-    # )
-
-    #
-
-    # cv2.imshow("Image", binary)
-    cv2.imshow("Image", cropped_image)
-    cv2.waitKey(0)
-
-    # Extract text
-    # psm=8, 6
-    extracted_text = pytesseract.image_to_string(
-        cropped_image,
-        lang="eng",
-        config="--oem 3 --psm 8 -c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
-        # config="--oem 3 --psm 11 -c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
-    )
 
     cropped_image = image[y : y + height, x : x + width]
 
     preprocessed_image = preprocess_image(cropped_image)
 
-    # # Extract text
+    # Extract text
     license_plate_text = extract_license_plate_text(preprocessed_image)
     return license_plate_text
-
-    return extracted_text
