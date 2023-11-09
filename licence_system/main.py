8e4b5595812ca51166d0fba879d2da1d9ef36692
Daniel <84752451+danielangeloni@users.noreply.github.com>
2023-11-09 11:50:55 +1100

    Add log for licence plate

--- licence_system/main.py
+++ licence_system/main.py
@@ -16,12 +16,13 @@ from licence_system.utils.log_plate import send_license_plate
 
 faulthandler.enable()
 
-# # Retrieve the LPR_CAMERA_ID environment variable
+# Retrieve the LPR_CAMERA_ID environment variable
 # CAMERA_ID = os.getenv("LPR_CAMERA_ID")
+CAMERA_ID = 1
 
-# # If the environment variable is not set, raise an exception
-# if CAMERA_ID is None:
-#     raise EnvironmentError("The TESSERACT_CMD environment variable is not set.")
+# If the environment variable is not set, raise an exception
+if CAMERA_ID is None:
+    raise EnvironmentError("The LPR_CAMERA_ID environment variable is not set.")
 
 inference_class = LPR_Inference(
     model_path="licence_system/models/checkpoints/LPLocalNet_B250_E500_LR0.0010_Acc74.22.pth",
@@ -45,8 +46,8 @@ if __name__ == "__main__":
             licence_plate = ocr_image(image)
             print(licence_plate)
 
-            # # Log plate to front end
-            # send_license_plate(licence_plate, CAMERA_ID)
+            # Log plate to front end
+            send_license_plate(licence_plate, CAMERA_ID)
 
             time.sleep(0.1)  # Add a short delay to not overwork the CPU
 
